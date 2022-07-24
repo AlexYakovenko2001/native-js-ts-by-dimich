b61214cfe1ada47c39b8b1b3a382061a4a57ce99
AlexYakovenko2001 <mrberrington2001@gmail.com>
2022-07-24 13:44:06 +0300

    Introduction to tests

---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,11 @@
+export const sum = (a: number, b: number) => (a + b);
+
+export const mult = (a: number, b: number) => (a * b);
+
+export const splitIntoWords = (sentence: string) => {
+    const word = sentence.toLowerCase().split(' ')
+    return word.filter(w => w !== '' && w !== '-').map(w => w
+        .replace('!', '')
+        .replace('.','')
+        .replace(',', ''))
+}
